458e213543a7b66822e4a416b876220174cb0e13
alisonzille <65909382+alisonzille@users.noreply.github.com>
2021-10-04 14:54:45 -0300

    Update __init__.py

--- antsys/__init__.py
+++ antsys/__init__.py
@@ -18,7 +18,7 @@
 ------------------------------------------------------------------------------
 '''
 __author__ = 'Alison Zille Lopes'
-__version__ = '0.1.24'
+__version__ = '0.1.26'
 from antsys.antsys import Edge
 from antsys.antsys import AntWorld
 from antsys.antsys import Ant
